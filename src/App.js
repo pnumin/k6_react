c08939052300ddfaa7a2f06c8e2bf3253219020c
pnumin <pnumin@pusan.ac.kr>
2024-03-12 09:16:29 +0900

    리액트시작하기

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
+    <>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           K-digital 6기 김경민
         </p>
+        <img src={logo} className="App-logo" alt="logo" />        
       </header>
     </div>
+    <div>402호</div>
+    </>
   );
 }
 
