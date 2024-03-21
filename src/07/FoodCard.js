80906935ee2fab699b26c4622919a11aed2d1b92
pnumin <pnumin@pusan.ac.kr>
2024-03-21 11:50:56 +0900

    food 분류목록

--- src/07/FoodCard.js
+++ src/07/FoodCard.js
@@ -1,13 +1,22 @@
 import bank from './img/bank.png' ;
 import market from './img/market.png' ;
 import busan from './img/busan.png' ;
+import { useState } from 'react';
 
 export default function FoodCard({fobj}) {
+  const [isClick, setIsClick] = useState(false) ;
+
   const fimg = fobj["구분"] === "기초푸드뱅크" ? bank :
-                fobj["구분"] === "기초푸드마켓" ? market : busan  
+                fobj["구분"] === "기초푸드마켓" ? market : busan  ;
+
+
+  const handleIsClick = () => {
+    setIsClick(!isClick);
+  }
   return (
     <div className='w-11/12 flex justify-center items-center
-                    p-1 border'>
+                    p-1 border'
+         onClick={handleIsClick}>
       <div className='w-1/6 max-w-xl flex justify-center items-center'>
         <img src={fimg} alt={bank} className='w-full inline-flex'/>
       </div>
@@ -21,9 +30,9 @@ export default function FoodCard({fobj}) {
         <h2 className='text-sm font-bold text-slate-400'>
           {fobj["사업장 소재지"]}
         </h2>
-        <div className='w-full text-sm
-                       bg-slate-600 text-white mt-5 p-2'>
-                        {`Tel : ${fobj["연락처(대표번호)"]} , Fax : ${fobj["팩스번호"]}`}
+        <div className='w-full h-8 text-sm
+                       bg-slate-600 text-white m-3 p-1'>
+        { isClick && `Tel : ${fobj["연락처(대표번호)"]} , Fax : ${fobj["팩스번호"]}`}
         </div>
       </div>
     </div>
--- src/07/FoodMain.js
+++ src/07/FoodMain.js
@@ -1,16 +1,40 @@
-import fdata from './fooddata.json' ;
+import fdata from './fooddata.json';
 import FoodCard from './FoodCard';
+import TailButton from '../UI/TailButton' ;
+
 export default function FoodMain() {
+  let c1 = fdata.map(item => item["운영주체 분류"]) ;
+  c1 = new Set(c1) ;
+  c1 = [...c1] ;
+  console.log(c1)
+
+  const handleList = (citem) => {
+    console.log(citem)
+  }
+
+  const c1Bts = c1.map(item => 
+    <TailButton caption ={item}  
+                color="blue"  
+                key = {item}
+                handleClick={() => handleList(item)} />
+  );
+
   const cards = fdata.map(item =>
-      <FoodCard fobj={item} key={item["사업장명"]}/>
-    );
+    <FoodCard fobj={item} key={item["사업장명"]} />
+  );
 
   return (
-    <div  className="w-full grid grid-cols-1 
+    <>
+      <div className='w-full bg-blue-100 p-5
+                      flex justify-center items-center'>
+        {c1Bts}
+      </div>
+      <div className="w-full grid grid-cols-1 
                       md:grid-cols-2 
                       xl:grid-cols-3 
                       gap-2">
-      {cards}
-    </div>
+        {cards}
+      </div>
+    </>
   )
 }
