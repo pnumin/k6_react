6babb3d2391819fffc920b7372a99207894a5f23
pnumin <pnumin@pusan.ac.kr>
2024-04-01 17:53:38 +0900

    일기예보

--- src/14/Frcst.js
+++ src/14/Frcst.js
@@ -1,10 +1,16 @@
 import TailInput from "../UI/TailInput" ;
 import TailSelect from "../UI/TailSelect";
 import TailButton from "../UI/TailButton";
+import getxy from "./getxy.json" ;
+
 import { useState, useEffect, useRef } from "react";
 
 export default function Frcst() {
-  const ops = ['부산','서울'] ;
+  const ops = getxy.map(item => item['1단계']) ;
+
+  const [x, setX] = useState();
+  const [y, setY] = useState();
+
   const dRef = useRef() ;
   const sRef = useRef() ;
 
@@ -13,8 +19,14 @@ export default function Frcst() {
   }
 
   const handleArea = () => {
-    console.log(sRef.current.value) ;
+    let tm = getxy.filter(item => item['1단계'] === sRef.current.value) ;
+    setX(tm[0]["격자 X"]) ;
+    setY(tm[0]["격자 Y"]) ;
   }
+
+  useEffect(() => {
+    console.log(x, y)
+  } , [x, y]) ;
   return (
     <div className="w-11/12 justify-start 
                     grid grid-cols-1 md:grid-cols-2 p-2 gap-2">
